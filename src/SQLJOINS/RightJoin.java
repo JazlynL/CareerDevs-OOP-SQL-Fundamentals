7335424f4b6e926490fb3fd05f252240286572a9
JazlynL <lubenjazlyn35@gmail.com>
2023-01-03 20:46:03 -0800

    Finally understanding  right join, took notes from youtube video

---
+++ src/SQLJOINS/RightJoin.java
@@ -0,0 +1,9 @@
+package SQLJOINS;
+
+public class RightJoin {
+    /*
+    * Right join will get only the full right side of the table, also return the information that does match */
+
+    //example
+    //select * from  Customer right join city where Customer.cityid = city.cityid;
+}
