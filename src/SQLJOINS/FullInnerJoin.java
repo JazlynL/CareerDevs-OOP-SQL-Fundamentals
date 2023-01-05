0cd9d9e3a9592201fdbc3205fc790533a3a83488
JazlynL <lubenjazlyn35@gmail.com>
2023-01-04 16:32:06 -0800

    Finally understanding full inner join, took notes from youtube video

---
+++ src/SQLJOINS/FullInnerJoin.java
@@ -0,0 +1,10 @@
+package SQLJOINS;
+
+public class FullInnerJoin {
+    /*
+    Full outer join its going to get the intersection
+    but it is also going to get all the data from both
+    sides of the table even if they're is no matching values.*/
+    //select * from customer full inner join city where  customer.cityid = city.cityid;
+
+}
