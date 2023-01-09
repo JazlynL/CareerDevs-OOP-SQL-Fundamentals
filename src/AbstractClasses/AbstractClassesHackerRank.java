78ab149fa7606128cb468864be9272cf63427fac
JazlynL <lubenjazlyn35@gmail.com>
2023-01-09 10:33:29 -0800

    finished hackerrank abstract algorithim.

--- src/AbstractClasses/AbstractClassesHackerRank.java
+++ src/AbstractClasses/AbstractClassesHackerRank.java
@@ -1,4 +1,19 @@
 package AbstractClasses;
 
 public class AbstractClassesHackerRank {
+    // create new class
+    class MyBook extends Book{
+
+
+
+        void setTitle(String s){
+            title= s;
+
+        }
+        String getTitle(String title){
+            System.out.println(title);
+            return title;
+        }
+
+    }
 }
